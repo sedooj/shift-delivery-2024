9367172bd0307562d3f273e34dc8e6dc474e4990
sedooj <sergo54ru@gmail.com>
2024-07-03 19:11:32 +0700

    Initial Commit

---
+++ shift-delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shift-delivery
+//
+//  Created by Сергей Герасимов on 03.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
